e6e8ff344d217c8eca9dfa80dba07c70f2faf68d
Rose22 <rosemelody22@gmail.com>
2025-09-06 20:43:56 +0200

    yay shapes!

--- oop_ex1/src/main.rs
+++ oop_ex1/src/main.rs
@@ -42,32 +42,85 @@ impl Shape2D {
         */
 
         // loop through every line (Y position)
-        for y in 0..=self.grid_size.y {
+
+        // get the shape's min and max Y
+        let mut min_y: i32 = 0;
+        let mut max_y: i32 = 0;
+        let mut min_y_set: bool = false;
+        for cursor_y in 0..=self.grid_size.y {
+            for vertice in &self.vertices {
+                if cursor_y == vertice.y {
+                    if !min_y_set {
+                        min_y = vertice.y;
+                        min_y_set = true;
+                    }
+
+                    max_y = vertice.y;
+                }
+            }
+        }
+
+        let mut min_x: i32 = 0;
+        let mut max_x: i32 = 0;
+        for cursor_y in 0..=self.grid_size.y {
             // within every line, loop through every horizontal position (X)
-            for x in 0..=self.grid_size.x {
-                let mut printed = false;
+
+            // first, get a line's min and max X so that we can fill up the shape
+            let mut min_x_set: bool = false;
+            for vertice in &self.vertices {
+                if cursor_y == vertice.y {
+                    if !min_x_set { min_x = vertice.x; min_x_set = true; }
+                    if vertice.x > max_x { max_x = vertice.x; }
+                }
+                // we preserve the min_x if a line has no vertices
+                // that way, it remembers
+            }
+
+            for cursor_x in 0..=self.grid_size.x {
+                let mut found_vert = false;
 
                 // on each position, check all vertices to see if any X&Y position matches
                 for vertice in &self.vertices {
-                    if x == vertice.x && y == vertice.y {
+                    if cursor_x == vertice.x && cursor_y == vertice.y {
                         // position match! print a character
-                        
-                        // use previous position to determine what character to draw
-                        print!("x");
-                        printed = true;
+                        print!("o");
+                        found_vert = true;
                     }
                 }
 
                 // fill up empty space when position doesn't match
-                if !printed { 
-                    print!(".");
+                if !found_vert { 
+                    if 
+                        cursor_x > min_x && cursor_x < max_x
+                        /*
+                        &&
+                        cursor_y > min_y && cursor_y < max_y 
+                        */
+                    {
+                        // we're inside the shape
+                        if cursor_y == min_y || cursor_y == max_y {
+                            // we're at the top or bottom edge
+                            print!("-");
+                        } else {
+                            // we're somewhere inbetween the left and right edge
+                            print!(".");
+                        }
+                    } else if cursor_x == min_x || cursor_x == max_x {
+                        print!("|");
+                    } else {
+                        // we're in blank space
+                        print!(" ");
+                    }
                 }
             }
+            print!(" minX:{min_x},maxX:{max_x}");
             println!();
         }
 
+        println!("min_y: {min_y}, max_y: {max_y}");
+
         for vertice in &self.vertices {
-            println!("vertice [{0}, {1}]", vertice.x, vertice.y);
+            println!("vertice [X:{0}, Y:{1}]", vertice.x, vertice.y);
         }
     }
 }
@@ -93,8 +146,8 @@ fn main() {
         vertices: vec![
             Position2D{x:0, y:0},
             Position2D{x:30, y:0},
-            Position2D{x:30, y:10},
             Position2D{x:0, y:10},
+            Position2D{x:30, y:10},
         ]
     };
     square.draw();
@@ -104,10 +157,11 @@ fn main() {
     let weird_shape = Shape2D{
         grid_size: Position2D{x:10, y:5},
         vertices: vec![
-            Position2D{x:0, y:0},
-            Position2D{x: 3, y: 0},
+            Position2D{x:2, y:0},
+            Position2D{x:4, y: 0},
+            Position2D{x:2, y:3},
             Position2D{x:5, y:3},
-            Position2D{x:0, y:3},
+            Position2D{x:1, y:5},
             Position2D{x:10, y:5},
         ]
     };
