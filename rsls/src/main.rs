165df92c8bb03557e8622e0a55269c7a2a3ea801
Rose22 <rosemelody22@gmail.com>
2025-09-07 05:36:54 +0200

    comments

--- rsls/src/main.rs
+++ rsls/src/main.rs
@@ -3,14 +3,21 @@ use std::{env, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    // check if enough args
     if args.len() < 2 {
         println!("usage: {0} <path>", args[0]);
         process::exit(1);
     }
 
-    for file in fs::read_dir(&args[1]).expect("invalid directory") {
-        let file = file.unwrap();
+    // get dir contents
+    let dir = fs::read_dir(&args[1]).unwrap_or_else(|e| {
+        println!("ERROR: {e}");
+        process::exit(1);
+    });
 
-        println!("{}", file.file_name().into_string().unwrap());
+    // list files in dir
+    for file in dir {
+        let file = file.expect("error reading file");
+        println!("{}", file.file_name().into_string().expect("error getting filename"));
     }
 }
